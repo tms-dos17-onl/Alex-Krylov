b534dd649fc4f653e3e106942f0f0e76783c4338
Alex777888777 <139115675+Alex777888777@users.noreply.github.com>
2023-07-29 12:31:34 +0300

    Update and rename Script 2.md to Script_for_task_2.sh

---
+++ HW_3/Script_for_task_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo useradd -m user_with_group4
+sudo touch user_with_group4 h
+sudo chmod 777 /home/user_with_group4/h
+sudo chmod 777 /home/user_with_group4
+sudo lshw -class processor >> /home/user_with_group4/h |echo ----- >> /home/user_with_group4/h 
+sudo free >> /home/user_with_group4/h |echo ------ >> /home/user_with_group4/h
+sudo ls -l /boot >> /home/user_with_group4/h|echo ----- >> /home/user_with_group4/h
+sudo lsblk >> /home/user_with_group4/h |echo ------- >> /home/user_with_group4/h
+
