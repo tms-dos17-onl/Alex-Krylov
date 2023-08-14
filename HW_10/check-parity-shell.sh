c0cb66f06d5788bb1182ffe63cb9d4c68c1fde27
Alex777888777 <139115675+Alex777888777@users.noreply.github.com>
2023-08-14 20:42:16 +0300

    Update check-parity-shell.sh

--- HW_10/check-parity-shell.sh
+++ HW_10/check-parity-shell.sh
@@ -13,18 +13,22 @@ elif [ $number =~ ^[0-9]+$ ] && [ $(($number%2)) -ne 0 ] ; then
  
 fi
 
-
 #!/bin/bash
+set -e 
 read number
 
  case $number in
      ''|*[0-9]*)
-     #if [ $(($number%2)) -eq 0 ] ; then
-echo even ;;
+     if [ $(($number%2)) -eq 0 ] ; then
+echo even 
+
+elif [ $(($number%2)) -ne 0 ] ; then
+echo odd 
+fi
+;;
+
   *)
-  echo no ;;
-   #elif [ $(($number%2)) -ne 0 ] ; then
- #echo 'odd' 
-# fi;;
+  echo not Num ;;
+  
   esac
  
