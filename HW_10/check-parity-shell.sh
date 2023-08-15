3b088b26400db62dbcacb97c37ad16221d385b22
Alex777888777 <139115675+Alex777888777@users.noreply.github.com>
2023-08-15 10:28:42 +0300

    Update check-parity-shell.sh

--- HW_10/check-parity-shell.sh
+++ HW_10/check-parity-shell.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-read number
-
-if [ $number =~ ^[0-9]+$ ]  && [ $(($number%2)) -eq 0 ] ; then
-echo 'even'
-
-elif [ $number =~ ^[0-9]+$ ] && [ $(($number%2)) -ne 0 ] ; then
- echo 'odd'
- else
- echo not Num
-
- exit 1
- 
-fi
-
 #!/bin/sh
 set -e 
 read number
