29120b3ccca889cdf1d49e98656bb614f4531b1d
Alex777888777 <139115675+Alex777888777@users.noreply.github.com>
2023-08-16 14:36:29 +0300

    Update check-parity-online.sh

--- HW_10/check-parity-online.sh
+++ HW_10/check-parity-online.sh
@@ -1,1 +1,14 @@
-gfh
+#!/bin/bash
+set -e 
+read number
+
+ case $number in
+     ''|*[!0-9]*)
+     echo not Num ;;
+  *)
+     [[ $(($number%2)) -eq 0 ]] && echo even || sleep 1
+     [[ $(($number%2)) -ne 0 ]] && echo odd  || sleep 1
+ ;;
+  
+  esac
+ exit 1
