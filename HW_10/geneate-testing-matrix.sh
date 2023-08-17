c5cb80e7fc819884011fe33a8e8fd68bdff581a8
Alex777888777 <139115675+Alex777888777@users.noreply.github.com>
2023-08-17 18:59:40 +0300

    Update geneate-testing-matrix.sh

--- HW_10/geneate-testing-matrix.sh
+++ HW_10/geneate-testing-matrix.sh
@@ -9,4 +9,10 @@ for an in ${!arch[@]} ; do
 echo "${os[$i]}" - "${arch[$an]}"
 done
 done
+---------------------------------
+array=(1 2 3 4 5 6 7)
+for i in $[$RANDOM % ${#array[@]}] ; do
+
+echo ${array[$i]}
+done
 
