47dc6cc6b77c94f02fcfb988033f17fc3bd2fb98
Alex777888777 <139115675+Alex777888777@users.noreply.github.com>
2023-08-25 15:09:10 +0300

    Update Script_for_task_2.sh

--- HW_3/Script_for_task_2.sh
+++ HW_3/Script_for_task_2.sh
@@ -3,7 +3,7 @@ sudo useradd -m user_with_group4
 sudo touch user_with_group4 h
 sudo chmod 777 /home/user_with_group4/h
 sudo chmod 777 /home/user_with_group4
-lshw -class processor | sudo tree -a /home/user_with_group4/h tree >> sudo | echo ----- >> /home/user_with_group4/h
+sudo lshw -class processor | sudo tree -a /home/user_with_group4/h >> sudo | sudo echo ----- >> /home/user_with_group4/h
 sudo free tree -a /home/user_with_group4/h >> sudo | echo ------ >> /home/user_with_group4/h
 sudo ls -l /boot tree -a /home/user_with_group4/h >> sudo | echo ----- >> /home/user_with_group4/h
 sudo lsblk tree -a /home/user_with_group4/h >> sudo | echo ------- >> /home/user_with_group4/h
