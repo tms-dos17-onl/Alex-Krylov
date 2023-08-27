43c529c6c50830fc547aa8a5d0f2fa9410e0bacb
Alex777888777 <139115675+Alex777888777@users.noreply.github.com>
2023-08-27 13:02:32 +0300

    Update Script_for_task_2.sh

--- HW_3/Script_for_task_2.sh
+++ HW_3/Script_for_task_2.sh
@@ -3,7 +3,7 @@ sudo useradd -m user_with_group4
 sudo touch user_with_group4 h
 sudo chmod 777 /home/user_with_group4/h
 sudo chmod 777 /home/user_with_group4
-sudo lshw -class processor | tee -a /home/user_with_group4/h 
+sudo lshw -class processor | tee -a /home/user_with_group4/h
 sudo free | tee -a /home/user_with_group4/h | sudo echo ------ | tee -a /home/user_with_group4/h
 sudo ls -l /boot tee -a /home/user_with_group4/h | echo ----- tee -a /home/user_with_group4/h
 sudo lsblk tee -a /home/user_with_group4/h | echo ------- tee -a /home/user_with_group4/h
