8186a0aa2f529f112d585d94abf4ed5aafb05e77
alex krylov <kullbro@gmail.com>
2023-08-29 13:49:10 +0300

    Update Script_for_HW_4.sh

--- HW_4/Script_for_HW_4.sh
+++ HW_4/Script_for_HW_4.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 Work() {
-<<<<<<< HEAD
-      echo 11111 >>/home/g
+    echo 11111 >> /home/g
     sleep 2 \\\\\\
-  Work
-=======
-  echo 11111 >> /home/g
- sleep 2 \\\\\\
- Work
->>>>>>> cc112fa (Update Script_for_HW_4.sh)
+    Work
 }
 Work
