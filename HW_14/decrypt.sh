957db203c183b9c972070688bea1c1f5b2437e6b
Alex777888777 <139115675+Alex777888777@users.noreply.github.com>
2023-09-07 15:59:15 +0300

    Update decrypt.sh

--- HW_14/decrypt.sh
+++ HW_14/decrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-openssl pkeyutl -encrypt -pubin -inkey /home/al/certificate/public.pem -in /home/al/certificate/sec.txt -out /home/al>
-openssl pkeyutl -decrypt -inkey /home/al/certificate/private.pem -in /home/al/certificate/sec1.txt -out /home/al/cert>
+openssl pkeyutl -encrypt -pubin -inkey /home/al/certificate/public.pem -in /home/al/certificate/sec.txt -out /home/al/certificate/sec1.txt
+openssl pkeyutl -decrypt -inkey /home/al/certificate/private.pem -in /home/al/certificate/sec1.txt -out /home/al/certificate/sec2.txt
