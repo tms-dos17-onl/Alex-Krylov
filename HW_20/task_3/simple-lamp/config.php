aae38ccbea8dd0952a56433de47dbd912f166179
Alex777888777 <139115675+Alex777888777@users.noreply.github.com>
2023-11-03 10:38:57 +0100

    Add files via upload

---
+++ HW_20/task_3/simple-lamp/config.php
@@ -0,0 +1,19 @@
+<?php
+// Database connection parameters
+$db_hostname = "db";
+$db_port     = 3306;
+$db_database = "simple_lamp";
+$db_username = "al";
+$db_password = "12345";
+
+// Image upload options
+$storage_option = "hd";
+$hd_folder  = "uploads";
+
+// Simulate latency, in seconds
+$latency = 0;
+
+// Cache configuration
+$enable_cache = false;
+$cache_server = "dns-or-ip-of-memcached-server";
+?>
